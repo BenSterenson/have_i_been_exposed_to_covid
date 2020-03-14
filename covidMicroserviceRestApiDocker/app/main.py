befe69487d7ac02f5cbdbafb930aff1e18fedb56
bensterenson <bensterenson@mail.tau.ac.il>
2020-03-14 12:23:00 +0200

    first commit

--- covidMicroserviceRestApiDocker/app/main.py
+++ covidMicroserviceRestApiDocker/app/main.py
@@ -1,7 +1,9 @@
 from connexion.resolver import RestyResolver
+from flask_cors import CORS
 import connexion
 
 if __name__ == '__main__':
     app = connexion.FlaskApp(__name__, port=9090, specification_dir='swagger/')
+    cors = CORS(app.app)
     app.add_api('covid.yaml', resolver=RestyResolver('app.api'))
     app.run()
